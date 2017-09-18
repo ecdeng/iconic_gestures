ecdeb227b8478ea2d11af5080af7fa7de5ce693f
Sampurna Basu <sampurna@Sampurnas-MacBook-Pro.local>
2017-09-17 21:05:59 -0700

    file explorer and drop down.

--- woonbot/Assets/Scenes/buttonScript.cs
+++ woonbot/Assets/Scenes/buttonScript.cs
@@ -17,11 +17,6 @@ public class buttonScript : MonoBehaviour {
 	{
 		Debug.Log("You have clicked the button!");
 		Texture2D texture = Selection.activeObject as Texture2D;
-		if (texture == null)
-		{
-			EditorUtility.DisplayDialog("Select Texture", "You must select a texture first!", "OK");
-			return;
-		}
 
 		string path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
 		if (path.Length != 0)
