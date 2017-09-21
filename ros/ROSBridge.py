76ff667d81beaa52c0f229ef749b6519471c7f65
Jeffrey Chen <chen305@usc.edu>
2017-09-21 20:56:45 +0100

    Serializing out points information from Unity now set up

--- ros/ROSBridge.py
+++ ros/ROSBridge.py
@@ -1,8 +1,39 @@
 import socket
+import json
+import traceback
+
+class Vector3(object):
+	"""docstring for Vector3"""
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+
+def parse_points (points):
+	pointsList = []
+	for point in points:
+		pointsList.append(Vector3(point['x'], point['y'], point['z']))
+	
+	return pointsList;
 
 def main():
-	TCPOutSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	TCPOutSock.connect(("localhost", 8888))
+	TCPSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	TCPSock.connect(("localhost", 8888))
+
+	try:
+		while True:
+			data = TCPSock.recv(1024)
+			points = json.loads(data);
+			pointsList = parse_points(points['points'])
+			print pointsList
+
+
+	except Exception:
+		traceback.print_exc()
+		TCPSock.close();
 
 if __name__ == '__main__':
 	main()
+
+
+
--- woonbot/Assets/Scenes/ROSBridge.cs
+++ woonbot/Assets/Scenes/ROSBridge.cs
@@ -2,31 +2,82 @@
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
+using System.Text;
+using System;
 using UnityEngine;
 
 public class ROSBridge : MonoBehaviour {
-    TcpListener serverSocket;
-    TcpClient clientSocket;
-    NetworkStream networkStream;
+    static TcpListener serverSocket;
+    static TcpClient clientSocket;
+    static NetworkStream networkStream;
+
 	// Use this for initialization
 	void Start () {
         serverSocket = new TcpListener(IPAddress.Any, 8888);
         clientSocket = default(TcpClient);
+        networkStream = null;
+
         serverSocket.Start();
         print("Server started");
+
+        Initialize();
+	}
+
+    void Initialize ()
+    {
+        Thread tid1 = new Thread(new ThreadStart(ROSBridge.CheckForConnection));
+        tid1.Start();
+    }
+
+    static void CheckForConnection()
+    {
         clientSocket = serverSocket.AcceptTcpClient();
         print("Client connected to");
-
         networkStream = clientSocket.GetStream();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        List<Vector3> points = new List<Vector3>();
+        points.Add(new Vector3(1, 2, 3));
+        points.Add(new Vector3(4, 5, 6));
+
+        sendPoints(points);
 	}
 
     void sendPoints(List<Vector3> points)
     {
+        string serializedPoints = UnityEngine.JsonUtility.ToJson(new Points(points));
+        Send(serializedPoints);
+    }
 
+    private void Send(string information)
+    {
+        if (networkStream == null) return;
+        try
+        {
+            byte[] sendBytes = Encoding.ASCII.GetBytes(information);
+            networkStream.Write(sendBytes, 0, sendBytes.Length);
+            networkStream.Flush();
+        }
+        catch (Exception ex)
+        {
+            print(ex.ToString());
+            networkStream.Close();
+            networkStream = null;
+            Initialize();
+        }
+    }
+}
+
+// Wrapper class to be able to serialize out the points
+public class Points
+{
+    public List<Vector3> points;
+
+    public Points(List<Vector3> points)
+    {
+        this.points = points;
     }
 }
