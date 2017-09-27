d0addd3389cdf6b214a3d6651d84d98d82a42884
SagarPunhani <sagar7p@gmail.com>
2017-09-27 10:27:59 -0700

    Fixing git issues

---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+public class ObjectManager : MonoBehaviour {
+
+	private GameObject gameObject;
+	Vector3[] vertices;
+	private bool togglepoints;
+	private List<GameObject> points;
+
+
+	void UpdateMesh(GameObject obj) {
+		var mesh = obj.GetComponentInChildren<MeshFilter> ().mesh;
+		var vertices = mesh.vertices;
+		var qangle = obj.transform.rotation;
+
+		//updating qangle
+		for(int i = 0; i < vertices.Length; i++)
+		{
+			vertices[i] = qangle * vertices[i];
+		}
+
+		//THIS IS FOR YOU ERIC
+		print (obj.transform.localScale.x);
+		for(int i = 0; i < vertices.Length; i++)
+		{
+			vertices[i] = obj.transform.localScale.x * vertices[i];
+			//vertices [i] = 0.1f * vertices[i];
+		}
+
+		mesh.vertices = vertices;
+
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		points = new List<GameObject> ();
+
+
+
+	}
+
+	public void ChangeObject(string name) {
+		Destroy (gameObject);
+
+		if (name=="sphere") {
+			gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			gameObject.transform.localScale = 0.1f * Vector3.one;
+		}
+		else {
+			gameObject = (GameObject)Instantiate(Resources.Load(name));
+		}
+		gameObject.transform.position = this.transform.position;
+		//gameObject.transform.localScale *= 3;
+
+		UpdateMesh (gameObject);
+
+		Mesh mesh = gameObject.GetComponentInChildren<MeshFilter> ().mesh;
+		vertices = mesh.vertices;
+
+		togglepoints = false;
+
+		foreach (var point in points) {
+			Destroy (point);
+		}
+			points.Clear ();
+
+		foreach (var vertex in vertices) {
+//			var v = vertex * 0.1f;
+			var gameObj = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			gameObj.transform.localScale = 0.01f * Vector3.one;
+			gameObj.transform.position = vertex;
+			gameObj.SetActive (true);
+			points.Add (gameObj);
+		}
+
+	}
+
+	void ApplyPhysics() {
+		
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+			
+		if (Input.GetKey ("up")) {
+			ChangeObject ("basketball");
+		}
+
+		if (Input.GetKey ("right")) {
+			ChangeObject ("sword");
+		}
+			
+		if (Input.GetKey("down")){
+			ChangeObject ("bowl");
+		}
+
+		if (Input.GetKey("left")){
+			ChangeObject ("pencil");
+		}
+
+		if (Input.GetKey("p")) {
+
+			//0 means no points 
+
+			togglepoints = !togglepoints;
+			foreach (var point in points) {
+				point.SetActive(togglepoints);
+			}
+
+			//1 means create
+
+
+			//2 means alive but dont create
+		}
+	}
+}
---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PointManager : MonoBehaviour {
+
+	public static List<Vector3> new_vertices;
+
+	void SelectPoints(int n) {
+		Mesh mesh = GetComponentInChildren<MeshFilter> ().mesh;
+		Vector3[] vertices = mesh.vertices;
+		var lvertices = new List<Vector3> (vertices);
+		new_vertices = new List<Vector3>(lvertices.Where((x, i) => i % (lvertices.Count/n) == 0));
+		foreach (var vertex in new_vertices) {
+			var gameObj = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			gameObj.transform.position = vertex;
+			gameObj.transform.localScale = Vector3.one * 0.01f;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		SelectPoints (100);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
