34946891e0a51290c3b8423b1617574dd02528ac
Jeffrey Chen <chen305@usc.edu>
2017-10-05 18:28:16 +0100

    cleanup

---
+++ ros/PointStream.py
@@ -0,0 +1,80 @@
+import socket
+import json
+import traceback
+from collections import deque
+import threading
+import thread
+import sys
+import signal
+
+class Point():
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z		
+
+	def toString(self):
+		return str(self.x) + " " + str(self.y) + " " + str(self.z)
+
+class PointStream(threading.Thread):
+	def __init__(self, ipAddress="localhost", port=8888):
+		threading.Thread.__init__(self)
+		self.__lock = threading.Lock()
+		self.__pointsListQueue = deque()
+		self.__ipAddress = ipAddress
+		self.__port = port
+
+	def __enqueue (self, points):
+		self.__lock.acquire()
+
+		try:
+			pointsList = []
+			for point in points:
+				pointsList.append(Point(point['x'], point['y'], point['z']))
+			self.__pointsListQueue.append(pointsList)
+		finally:
+			self.__lock.release()
+
+	def dequeue(self):
+		self.__lock.acquire()
+
+		try:
+			if self.__pointsListQueue:
+				return self.__pointsListQueue.popleft()
+			else:
+				return
+		finally:
+			self.__lock.release()
+
+	def empty(self):
+		self.__lock.acquire()
+
+		try:
+			if self.__pointsListQueue:
+				return False
+			else:
+				return True
+		finally:
+			self.__lock.release()
+
+	def start(self):
+		thread.start_new_thread(self.run, ())
+
+	def run(self):
+		while True:
+			try:
+				TCPSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				TCPSock.connect((self.__ipAddress, self.__port))
+				break
+			except:
+				TCPSock.close()
+
+		try:
+			while True:
+				data = TCPSock.recv(1024)
+				points = json.loads(data);
+				self.__enqueue(points['points'])
+
+		except Exception:
+			traceback.print_exc()
+			TCPSock.close()
