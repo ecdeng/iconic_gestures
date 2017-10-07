c35cb63054e518d9f799517435148874fb071a58
Sampurna Basu <sampurna@Sampurnas-MacBook-Pro.local>
2017-10-07 16:38:36 -0700

    UI overhaul

---
+++ Assets/ControlPanelScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+	public GameObject Panel;
+
+	// Use this for initialization
+	void Start () {
+		Panel.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void showPanel() {
+		Panel.gameObject.SetActive (true);
+	}
+
+	public void hidePanel() {
+		Panel.gameObject.SetActive (false);
+	}
+}
---
+++ Assets/SettingsButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Control_Panel : MonoBehaviour {
+
+	public Button myButton;
+	private GameObject controlPanel;
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		myButton.onClick.AddListener(delegate {
+			TaskOnClick(myButton);
+		});
+		controlPanel = GameObject.Find ("Control_Panel");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//make control panel appear
+	void TaskOnClick(Button target)
+	{
+		Debug.Log ("hello is this working");
+		controlPanel.SetActive(false);
+	}
+}
