88ce96bbf51991bd25b3d9d18ca43c6d2d2751d4
Rebecca Chen <chen676@usc.edu>
2017-10-11 15:24:54 -0700

    stick model

--- Assets/Scripts/Gestures/StickMoveManager.cs
+++ Assets/Scripts/Gestures/StickMoveManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,27 +16,46 @@ public class StickMoveManager : MoveManager {
 	List<Vector3> GetStickPoints()
 	{
 
-		var allverts = this.GetVertices (0);
-		Vector3 min = allverts[0];
-		Vector3 max = allverts [1];
-		foreach (var ver in allverts) {
-			if (ver.y < min.y) {
-				min = ver;
-			} else if (ver.y > max.y) {
-				max = ver;
+		var allVertices = this.GetVertices (0);
+		Vector3 minY = allVertices [0]; // lower endpoint
+		Vector3 maxY = allVertices [0]; // upper endpoint
+
+		// get the min and max x and y values
+		foreach (var ver in allVertices) {
+			if (ver.y < minY.y) {
+				minY = ver;
+			} else if (ver.y > maxY.y) {
+				maxY = ver;
 			}
 		}
+		float middleYValue = ((maxY.y + minY.y) / 2);
 
-		Vector3 avg1 = new Vector3 (min.x - 0.05f, (min.y + max.y) / 2, min.z);
-		Vector3 avg2 = new Vector3 (min.x + 0.05f, (min.y + max.y) / 2, min.z);
+		Vector3 minX = allVertices [0]; // lower endpoint
+		Vector3 maxX = allVertices [0]; // upper endpoint
 
-
+		// get the points with max/min x values around the middle Y value
+		foreach (var ver in allVertices) {
+			if (Math.Abs (ver.y - middleYValue) <= .09) {
+				if (ver.x < minX.x) {
+					minX = ver;
+				} else if (ver.x > maxX.x) {
+					maxX = ver;
+				}
+			}
+		}
+			
+//		Vector3 avg1 = new Vector3 (min.x - 0.05f, (min.y + max.y) / 2, min.z);
+//		Vector3 avg2 = new Vector3 (min.x + 0.05f, (min.y + max.y) / 2, min.z);
+//
 		List<Vector3> points = new List<Vector3>();
-		points.Add(min);
-		points.Add(avg1);
-		points.Add (max);
-		points.Add (avg2);
-
+//		points.Add(min);
+//		points.Add(avg1);
+//		points.Add (max);
+//		points.Add (avg2);
+		points.Add(minY);
+		points.Add (maxY);
+		points.Add (minX);
+		points.Add (maxX);
 		return points;
 	}
 
