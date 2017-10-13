4ce465a2f5347968c00b3db9531528b4fb5d8d67
Sampurna Basu <sampurna@Sampurnas-MacBook-Pro.local>
2017-10-13 11:08:53 -0700

    box and book move managers.

--- Assets/Scripts/Gestures/BoxMoveManager.cs
+++ Assets/Scripts/Gestures/BoxMoveManager.cs
@@ -22,22 +22,72 @@ public class BoxMoveManager : MoveManager {
 		new_vertices.Add(new Vector3(boundPoint2.x, boundPoint1.y, boundPoint2.z));
 		new_vertices.Add(new Vector3(boundPoint2.x, boundPoint2.y, boundPoint1.z));
 
+		var angle = transform.rotation;
+		var scale = transform.localScale;
+		var position = transform.position;
+
 		for (var i = 0; i < new_vertices.Count; i++) {
-			new_vertices [i] += transform.position;
+//			new_vertices[i] = angle * new_vertices[i];
+//			new_vertices[i] = new Vector3(new_vertices[i].x*scale.x,new_vertices[i].y*scale.y,new_vertices[i].z*scale.z);
+			new_vertices [i] += position;
+
+		}
+		//return new_vertices;
+
+		//var allVertices = this.GetVertices (0);
+		float minX = new_vertices [0].x;
+		float minY = new_vertices [0].y;
+		float minZ = new_vertices [0].z;
+		float maxX = new_vertices [0].x;
+		float maxY = new_vertices [0].y;
+		float maxZ = new_vertices [0].z;
+
+		List<Vector3> points = new List<Vector3>();
+
+		// get the min/max x/y/z values
+		foreach (var ver in new_vertices) {
+			if (ver.x < minX) {
+				minX = ver.x;
+			}
+			if (ver.x > maxX) {
+				maxX = ver.x;
+			} 
+			if (ver.y < minY) {
+				minY = ver.y;
+			}
+			if (ver.y > maxY) {
+				maxY = ver.y;
+			}
+			if (ver.z < minZ) {
+				minZ = ver.z;
+			}
+			if (ver.z > maxZ) {
+				maxZ = ver.z;
+			}
+			//points.Add (ver);
 		}
 
-		return new_vertices;
+		float midX = (float) (maxX + minX) / (float) 2;
+		float midY = (float) (maxY + minY) / (float) 2;
+		float midZ = (float) (maxZ + minZ) / (float) 2;
+
+
+		points.Add (new Vector3(midX, midY, minZ)); //front face
+		points.Add (new Vector3(midX, midY, maxZ)); //back face
+		points.Add (new Vector3(midX, minY, midZ)); //bottom face
+		points.Add (new Vector3(midX, maxY, midZ)); // top face
+		points.Add (new Vector3(minX, midY, midZ)); //left face
+		points.Add (new Vector3(maxX, midY, midZ)); //right face
+		return points;
 
 	}
 
 	// Use this for initialization
 	void Start () {
-
 		base.Start ();
 		vertices = GetBoxVertices();
 		ShowPoints ();
 		StartCoroutine (base.Move(2));
-
 	}
 	
 	// Update is called once per frame
