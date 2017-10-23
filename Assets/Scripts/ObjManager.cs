d4af3cfc22f47287a35824b82e1101340653c798
SagarPunhani <sagar7p@gmail.com>
2017-10-23 10:55:55 -0700

    overhauled project

---
+++ Assets/Scripts/ObjManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjManager : Singleton<ObjManager> {
+
+	public GameObject model;
+	private float movespeed = 1.0f;
+	private float scale = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		var filepath = "Assets/Models/pikachu.obj";
+		LoadModel (filepath);
+	}
+
+	void LoadModel(string filepath) {
+		model = OBJLoader.LoadOBJFile (filepath);
+		model.transform.parent = transform;
+		model.transform.position = transform.position + new Vector3(0,transform.localScale.y,0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (model != null) {
+			if (Input.GetKey ("left")) {
+				model.transform.Rotate(0, movespeed, 0);
+			}
+			if (Input.GetKey ("right")) {
+				model.transform.Rotate(0, -1*movespeed, 0);
+			}
+			if (Input.GetKeyDown ("r")) {	
+				Vector3 rot = model.transform.rotation.eulerAngles;
+				rot = new Vector3(rot.x,rot.y+180,rot.z);
+				model.transform.rotation = Quaternion.Euler(rot);
+			}
+
+			if (Input.GetKey ("up")) {
+				model.transform.localScale *= (1 + scale);
+			}
+			if (Input.GetKey ("down")) {
+				model.transform.localScale *= (1 - scale);
+
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	public static T Instance;
+	void Awake() {
+		Instance = (T) FindObjectOfType(typeof(T));
+	}
+}
