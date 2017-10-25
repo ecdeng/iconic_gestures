92f03981a953c313b275b70879d116244f757cc0
SagarPunhani <sagar7p@gmail.com>
2017-10-25 10:44:16 -0700

    obj stuff

--- Assets/Scripts/ObjManager.cs
+++ Assets/Scripts/ObjManager.cs
@@ -9,12 +9,46 @@ public class ObjManager : Singleton<ObjManager> {
 	private float scale = 0.05f;
 	private Dictionary<int,GameObject> point_ids;
 
+	Vector2 scrollPosition = Vector2.zero;
+
+
+	void OnGUI () {
+		string[] listItems = 
+		{
+			"Hello world,",
+			"this",
+			"is a",
+			"very",
+			"very",
+			"very",
+			"very",
+			"very",
+			"very",
+			"long",
+			"list.",
+		};
+
+		GUILayout.BeginArea(new Rect(0f, 0f, 300f, 200f), GUI.skin.window);
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true); 
+		GUILayout.BeginVertical(GUI.skin.box);
+
+		foreach (string item in listItems)
+		{
+			GUILayout.Label(item, GUI.skin.box, GUILayout.ExpandWidth(true));
+		}
+
+		GUILayout.EndVertical();
+		GUILayout.EndScrollView();
+		GUILayout.EndArea();
+	}
 
 	// Use this for initialization
 	void Start () {
 		var filepath = "Assets/Models/pikachu.obj";
 		point_ids = new Dictionary<int, GameObject> ();
 		LoadModel (filepath);
+
+
 	}
 		
 	public void LoadModel(string filepath) {
