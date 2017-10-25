298f91a6216140a1ab1c060588e35736b3866a0b
Rebecca Chen <chen676@usc.edu>
2017-10-25 02:12:16 -0700

    did it the actual way without GUILayout

---
+++ Assets/Scripts/ListItemScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListItemScript : MonoBehaviour {
+	public Text text;
+	public int id;
+	public float x;
+	public float y;
+	public float z;
+	public bool selected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
