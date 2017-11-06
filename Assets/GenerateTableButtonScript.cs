fd65e4f88fa344be20c39f43111b417e22a29ce3
Sampurna Basu <sampurna@Sampurnas-MacBook-Pro.local>
2017-11-05 21:34:27 -0800

    fixed

--- Assets/GenerateTableButtonScript.cs
+++ Assets/GenerateTableButtonScript.cs
@@ -10,37 +10,38 @@ public class GenerateTableButtonScript : MonoBehaviour {
 	public GameObject ListItemPrefab;
 	private GameObject model;
 	public bool showTable = false;
-	public int numRows = 3;
+	public int numRows = 2;
 	public int numCols = 2;
 	public string str = "";
 	List<string> inputVals;
 
 	// Use this for initialization
 	void Start () {
+		
+		Debug.Log ("START");
 		inputVals = new List<string> ();
 		Button btn = generateTableButton.GetComponent<Button>();
-		btn.onClick.AddListener(delegate{GenerateTable(2,2);});
+		btn.onClick.AddListener(GenerateTable);
 		TablePanel = GameObject.Find ("TablePanel");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	void OnGUI(){
-		Debug.Log ("ONGUI");
-		//for testing
-
-		//
+		//Debug.Log ("ONGUI");
+		
 		if (showTable){
-
+			
 			GUILayout.BeginArea(new Rect(Screen.width - numCols * 150, 0 , numCols*150, numRows*40), GUI.skin.window);
+			Debug.Log ("numCols:" + numCols + " numRows: " + numRows);
 
 			for (int i = 0; i < numRows; i++) {
 				GUILayout.BeginHorizontal ();
 				for (int j = 0; j<numCols; j++) {
-					inputVals[i*numRows + j] = GUILayout.TextField (inputVals[i*numRows + j], 3);
+					inputVals[i*numCols + j] = GUILayout.TextField (inputVals[i*numCols + j], 3);
+
 				}
 				GUILayout.EndHorizontal ();
 			}
@@ -51,10 +52,11 @@ public class GenerateTableButtonScript : MonoBehaviour {
 
 	}
 
-	public void GenerateTable(int rows, int cols) {
+	public void GenerateTable() {
 		Debug.Log ("GENERATE TABLE");
 		showTable = true;
-
+		numRows = 10;
+		numCols = 2;
 		int invalid = -1;
 		for (int i = 0; i<numRows; i++) {
 			for (int j = 0; j<numCols; j++) {
